40fa537c3a96f666977a9a98563908b97e8c221f
Saiju <saijumathew@gmail.com>
2021-02-13 14:42:28 -0500

    dbo operations

--- code/helper/dbo.js
+++ code/helper/dbo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const ModelUsers = require('./ModelUser');
+const ModelQA = require('./ModelQA');
 
 //Connection String to MongoDB in the cloud
 const connectionstring =  "mongodb://mike:denton@"
@@ -14,7 +15,7 @@ class DBO {
     constructor (){
         //Setup DB
         mongoose
-            .connect(connectionstring, {useNewUrlParser:true})
+            .connect(connectionstring, {useNewUrlParser:true, useFindAndModify: false })
             .then(()=>{console.log('Mongoose connected successfully');},
             error => {console.log("Mongoose could not connect to database:"+error)}
         )
@@ -30,7 +31,7 @@ class DBO {
             TokenBalance:50
         })
         newUser.save(function(err){
-            if(err) console.log(`Error occurred in adding user to DB():${error}`)
+            if(err) console.log(`Error occurred in adding user to DB():${err}`)
             else {
                 console.log(`Successfully added user to DB`);
             }
@@ -47,6 +48,63 @@ class DBO {
         const User = await ModelUsers.findOne({UserName:_username});
         //console.log(all);
         return User;
+    }
+
+    createQuestion = (question) =>{
+        console.log('start create question');
+        const newQuestion = new ModelQA.question({
+            UserName:question.username,
+            QuestionText:question.question,
+            QuestionCost:question.cost
+        });
+
+        newQuestion.save(function(err){
+            if(err) console.log(`Error occurred in adding question to DB():${err}`)
+            else {
+                console.log(`Successfully added question to DB`);
+            }
+        });
+    }
+
+    findAllQuestions = async () =>{
+        const AllQuestions = await  ModelQA.question.find({});
+        //console.log(all);
+        return AllQuestions;
+    }
+
+    findQuestionByID = async (qid) =>{
+        const Question = await ModelQA.question.findById(qid);
+        return Question;
+    }
+
+    saveAnswerToQuestion =  (answer) =>{
+        ModelQA.question.findById(answer.qid, (err, result) =>{
+            var newAnswer = new ModelQA.answer({
+                UserName:answer.username,
+                AnswerText:answer.answer
+            });
+            result.Answers.push(newAnswer);
+            result.save(function(err){
+                if(err) console.log(`Error occurred in adding answer to DB():${err}`)
+                else {
+                    console.log(`Successfully added answer to DB`);
+                }
+            });
+        });
+    }
+
+    declareAnswerWinner = (winner) =>{
+        console.log("DeclareWinner");
+        ModelQA.question.findOneAndUpdate(
+            { "_id": winner.qid, "Answers._id": winner.aid },
+            { "Answers.$.IsWinner" : true },
+            function(err,doc) {
+                if(err) console.log(`Error occurred in declaring winner answer to DB():${err}`)
+                else {
+                    console.log(`Successfully added declaring winner answer to DB`);
+                }
+            }
+        );        
     }
 
 }
