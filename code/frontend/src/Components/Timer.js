38925c855b9239c029e0f6088ae585068629de9c
Elanie <elanietquiambao@gmail.com>
2021-02-14 05:32:26 -0500

    installed react-moment for Timer but show errors

--- code/frontend/src/Components/Timer.js
+++ code/frontend/src/Components/Timer.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Moment from "react-moment";
+// import Moment from "react-moment";
 
 const Timer = () => {
     return ( 
         <>
-            <Moment format="hh:mm:ss" style={{margin: '15px'}}>
+            {/* <Moment format="hh:mm:ss" style={{margin: '15px'}}>
                 {Moment.moment }  
-            </Moment>
+            </Moment> */}
         </>
     );
 }
